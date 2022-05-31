86aeb95fceeb09e92dc898ef71569938b4c246c6
chalex <alejandrelee+magbogbro@gmail.com>
2022-05-30 23:23:46 -0400

    add chlx merro60

---
+++ keyboards/chlx/merro60/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4E, 0xDA, 0x26, 0x8A, 0x47, 0x36, 0xF5, 0xE9}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
